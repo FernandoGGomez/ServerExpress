cdec18ce564a39a912dca77a8a37303abe500145
Fernando Gómez <fernandogomez03012000@gmail.com>
2023-08-08 16:37:24 -0300

    Agregue el .bind en unos métodos

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -8,17 +8,17 @@ route.use(express.urlencoded({extended: true}));
 
 route.post("/register",passport.authenticate("register",{session:false,failureRedirect:"/api/auth/failureregister"}),passport.authenticate("login"),  controller.register.bind(controller))
 
-route.get("/failureregister",controller.failureRegister)
+route.get("/failureregister",controller.failureRegister.bind(controller))
 
-route.post("/login",passport.authenticate("login"), controller.login)
+route.post("/login",passport.authenticate("login"), controller.login.bind(controller))
 
-route.get("/failurelogin",controller.failurelogin)
+route.get("/failurelogin",controller.failurelogin.bind(controller))
 
 route.get("/github",passport.authenticate("github", {scope:["user:email"]}),(req,res)=>{})
 
-route.get("/github-callback",passport.authenticate("github" ,{failureRedirect:"/login"}),controller.githubCallback)
+route.get("/github-callback",passport.authenticate("github" ,{failureRedirect:"/login"}),controller.githubCallback.bind(controller))
 
-route.post("/logout",controller.logout)
+route.post("/logout",controller.logout.bind(controller))
 
 route.post("/restorepassword",controller.restorePassword.bind(controller))
 
